2f27454c0bac7967fa0dc5af47485f16b23b5ede
Marantle <leri6@hotmail.com>
2018-10-13 02:13:03 +0300

    dist to gitignore

--- test/App.ts
+++ test/App.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 import App from '../src/App'
 // TODO refer to https://medium.com/@FizzyInTheHall/run-typescript-mocha-tests-in-visual-studio-code-58e62a173575
+// TODO and https://hackernoon.com/testing-node-js-in-2018-10a04dd77391
 describe('App sayHello', () => {
   it('should say "hello"', () => {
     const text = App.sayHello()
