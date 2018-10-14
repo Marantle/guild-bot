853258f7c076d297f441805db86dc805a4531c21
Marantle <leri6@hotmail.com>
2018-10-14 18:57:07 +0300

    update circleci

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,33 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+  // entry is where, say, your app starts - it can be called main.ts, index.ts, app.ts, whatever
   entry: ['./src/index.ts'],
+
+  // This forces webpack not to compile TypeScript for one time, but to stay running, watch for file changes in project directory and re-compile if needed
+
+
+  // Is needed to have in compiled output imports Node.JS can understand. Quick search gives you more info
   target: 'node',
+
+  // Prevents warnings from TypeScript compiler
+  externals: [
+    nodeExternals({
+      whitelist: [
+        'webpack/hot/poll?100',
+        /^discord.js/,
+        /^winston/,
+        /^app-root-path/,
+        /^firebase-admin/
+      ],
+    }),
+  ],
+
   module: {
     rules: [{
       test: /.tsx?$/,
-      use: 'ts-loader'
+      use: 'ts-loader',
+      exclude: /node_modules/,
     }],
   },
   resolve: {
@@ -18,6 +39,9 @@ module.exports = {
       Config: path.resolve(__dirname, 'config/'),
     },
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'server.js',
